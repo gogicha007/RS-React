687c6c1ea17d874b2f7b6b57ab3d44fdde86f89e
gogicha007 <gogicha@gmail.com>
2024-12-27 16:15:25 +0400

    feat: add testing codes

---
+++ rs-react-app/src/components/errorButton/errorButton.test.tsx
@@ -0,0 +1,10 @@
+import {render} from "@testing-library/react"
+import ErrorButton from "./errorButton"
+
+
+describe("Error Button", ()=>{
+    test('renders correctly', ()=>{
+        render(<ErrorButton/>)
+        
+    })
+})
