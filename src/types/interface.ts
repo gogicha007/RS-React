ea37b604da479b91326ffb6a364140673692f956
gogicha007 <gogicha@gmail.com>
2025-01-01 15:25:25 +0400

    chore: save the progress

--- src/types/interface.ts
+++ src/types/interface.ts
@@ -1,11 +1,3 @@
-export interface IFData {
-  name: string;
-  url: string;
-  status: string;
-  gender: string;
-  image: string;
-}
-
 export interface IFCharacter {
   id: number;
   name: string;
