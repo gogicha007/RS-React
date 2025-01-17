dc70ea1c556ff6962d0bfbb178d8a7557c60f73f
gogicha007 <gogicha@gmail.com>
2025-01-17 14:34:44 +0400

    chore: save the progress

--- src/App.tsx
+++ src/App.tsx
@@ -8,15 +8,13 @@ import {
 } from 'react-router-dom';
 import NotFound from './pages/notfound/notfound';
 import { Details, detailsLoader } from './components/details/details';
-// import ResultsLayout from './layouts/ResultsLayout';
-// import { resultsLoader } from './layouts/ResultsLayout';
-import Results, { resultsLoader } from './components/results/results';
+import Results from './components/results/results';
 
 function App() {
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path="/" element={<RootLayout />}>
-        <Route path="results" element={<Results />} loader={resultsLoader}>
+        <Route path="results" element={<Results />}>
           <Route path=":id" element={<Details />} loader={detailsLoader} />
         </Route>
         <Route path="*" element={<NotFound />} />
--- src/components/results/results.tsx
+++ src/components/results/results.tsx
@@ -1,17 +1,35 @@
-import type { Params } from 'react-router';
+// import type { Params } from 'react-router';
 import styles from './results.module.css';
 import { IFCharacter } from '../../types/interface';
 import { Card } from '../card/card';
 import { Link, Outlet, useLocation } from 'react-router';
+import { getList } from '../../utils/fetcher';
+import { useEffect, useState } from 'react';
+import { IFResponse } from '../../types/interface';
 
-function Results() {
+const Results = () => {
+  const [results, setResults] = useState([] as IFCharacter[]);
+
   const { state } = useLocation();
   const data = state?.data || [];
-  console.log(state?.pagination || 'null');
+
+  console.log(data);
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const res = await getList('Dead');
+        setResults((res as IFResponse).results);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    fetchData();
+  }, []);
+  console.log('setResults', results);
   return (
     <div className={styles.results}>
       <div className={styles.results__list}>
-        {data.map((obj: IFCharacter) => {
+        {results.map((obj: IFCharacter) => {
           return (
             <Link to={obj.id.toString()} key={obj.id}>
               <Card {...obj} />
@@ -22,10 +40,12 @@ function Results() {
       <Outlet />
     </div>
   );
-}
+};
 
 export default Results;
 
-export async function resultsLoader({ params }: { params: Params }) {
-  console.log(params);
-}
+// export async function resultsLoader({ params }: { params: Params }) {
+//   // const data = await getList('Dead')
+//   // console.log('results',data);
+//   // return data
+// }
